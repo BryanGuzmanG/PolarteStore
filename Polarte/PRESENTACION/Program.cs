8d60c709be50db5143cbfa42626765ac27858c2c
Bryan Guzman <bryanguzman18@outlook.com>
2019-11-18 20:06:58 -0900

    .

--- Polarte/PRESENTACION/Program.cs
+++ Polarte/PRESENTACION/Program.cs
@@ -16,8 +16,9 @@ namespace PRESENTACION
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new frmLogin());
-            Application.Run(new PantallaPrincipal());
+            
+              Application.Run(new frmLogin());
+            //Application.Run(new PantallaPrincipal());
             //Application.Run(new frmProducto());
 
             
